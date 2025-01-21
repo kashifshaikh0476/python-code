506314c3fe9bac22d629eb6883e19fe590869e1b
kashif shaikh <shaikhkashif8684@gmail.com>
2025-01-21 08:54:06 +0530

    Add files via upload

---
+++ nestedifelse.py
@@ -0,0 +1,36 @@
+#i =10
+#if(i ==10):
+
+ #   if(i < 20):
+  #      print ("i is smaller than 20")
+#if (i < 15):
+ #   print ("i is smaller than 15 too")
+#else:
+ #   print ("i is greater than 15")
+
+#letter="A"
+#if letter=="B":
+ #   print("letter is b")
+#else:
+#   if letter=="c":
+#      print("letter is c")
+# else:
+#    if letter=="A":
+#       print("letter is A")
+#  else:
+#     print("letter is not a,b,c")
+
+
+
+#nested if with use of elif:            
+letter = "A"
+
+if letter == "B":
+
+    print("letter is b")
+elif letter == "C":
+    print("letter is c")
+elif letter == "A":
+    print("letter is A")
+else:
+   print("letter is not a, b, c")
